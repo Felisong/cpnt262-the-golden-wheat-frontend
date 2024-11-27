84430999eb949bdaf8f782fb50de311ebf06e55b
Felisong <carolinahs100@gmail.com>
2024-11-27 15:31:52 -0700

    all form variables declared.

--- app/components/LogIn.js
+++ app/components/LogIn.js
@@ -1,4 +1,9 @@
+import { Work_Sans } from "next/font/google";
 import { useState } from "react";
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+});
 
 export default function LogIn() {
   // variables
@@ -8,27 +13,49 @@ export default function LogIn() {
   const [email, setEmail] = useState("");
   const [validateForm, setValidateForm] = useState("");
 
+  // are conditions true?
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isPassConfirmValid, setIsPassConfirmValid] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
   // errors
   const [usernameErr, setUsernameErr] = useState("");
   const [passwordErr, setPasswordErr] = useState("");
   const [passwordConfirmErr, setPasswordConfirmErr] = useState("");
   const [emailErr, setEmailErr] = useState("");
 
+  // functions for conditions
+  function usernameCondition(currentName) {
+    if (currentName.length < 3 || currentName.length === 0) {
+      setUsernameErr("Please make name longer than 3 characters.");
+      return false;
+    } else {
+      setUsernameErr("");
+    }
+  }
+
   return (
-    <form className="w-full max-w-lg">
+    <form className={`w-full max-w-lg p-4 ${workSans.className} bg-yellow-100`}>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-first-name"
           >
-            First Name
+            User Name
           </label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="grid-first-name"
             type="text"
             placeholder="Jane"
+            onChange={(e) => {
+              const value = e.target.value;
+              setUserName(value);
+            }}
           />
           <p className="text-red-500 text-xs italic">
             Please fill out this field.
@@ -39,13 +66,13 @@ export default function LogIn() {
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-last-name"
           >
-            Last Name
+            Email
           </label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="text"
-            placeholder="Doe"
+            placeholder="emailhere@gmail.com"
           />
         </div>
       </div>
@@ -68,63 +95,26 @@ export default function LogIn() {
           </p>
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-city"
+            htmlFor="grid-password"
           >
-            City
+            Confirm Password
           </label>
           <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-city"
-            type="text"
-            placeholder="Albuquerque"
-          />
-        </div>
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-state"
-          >
-            State
-          </label>
-          <div className="relative">
-            <select
-              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-state"
-            >
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-zip"
-          >
-            Zip
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-zip"
-            type="text"
-            placeholder={90210}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="password"
+            placeholder="******************"
           />
         </div>
       </div>
+
+      <button className="p-4 rounded bg-transparent border-yellow-400 ">
+        Submit
+      </button>
     </form>
   );
 }
