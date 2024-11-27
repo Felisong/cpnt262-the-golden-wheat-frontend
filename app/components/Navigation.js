1951be8f1e43eefb5cbe5ce0a17536ce34527d65
Felisong <carolinahs100@gmail.com>
2024-11-27 09:38:48 -0700

    test

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -6,6 +6,10 @@ export default function Navigation() {
         alt="logo of the Golden Wheat, it is pink with a yellow wheat in the center"
       />
       <h2 className="${lexend.className} ">THE GOLDEN WHEAT FACTORY</h2>
+
+      <ul>
+        <li> test</li>
+      </ul>
     </nav>
   );
 }
