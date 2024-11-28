d4a57b63e74641099b080ab849a44ad3350d32ac
Felisong <carolinahs100@gmail.com>
2024-11-28 00:20:52 -0700

    header will be done as soon as I import some things in.

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Hamburger from "./Hamburger";
 import Logo from "./Logo";
 import { Lexend_Exa } from "next/font/google";
+import Link from "next/link";
 const lexend = Lexend_Exa({
   subsets: ["latin"],
 });
@@ -11,11 +12,14 @@ const lexend = Lexend_Exa({
 export default function Navigation() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  const navLinks = [
-    { text: "Home", url: "/" },
-    { text: "About", url: "/about" },
-    { text: "Menu", url: "/products" },
+  const navigation = [
+    { name: "Home", href: "/", current: true, id: 1 },
+    { name: "About", href: "/about", current: false, id: 2 },
+    { name: "Menu", href: "/products", current: false, id: 3 },
   ];
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
     <nav
@@ -33,6 +37,24 @@ export default function Navigation() {
         <li>
           <Hamburger />
         </li>
+        <div className="flex space-x-4 w-40 ">
+          {/* GET NAVIGATION WORKING PRIO */}
+          {navigation.map((item) => (
+            <Link
+              key={item.id}
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-darkBlue text-white px-2 text-2xl w-30"
+                  : "text-yellowBright hover:bg-gray-700 hover:text-white",
+                "rounded-md py-2"
+              )}
+            >
+              {item.text}
+            </Link>
+          ))}
+        </div>
       </ul>
     </nav>
   );
