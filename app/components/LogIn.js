d4c0927389fb199dd33f83b9644b9d9f4a0586bd
Felisong <carolinahs100@gmail.com>
2024-11-28 13:35:57 -0700

    got form to work on the button, and am now adding router to work.

--- app/components/LogIn.js
+++ app/components/LogIn.js
@@ -1,6 +1,7 @@
 import { Work_Sans } from "next/font/google";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { Router } from "next/router";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -188,29 +189,31 @@ export default function LogIn() {
     <form className={` max-w-fit p-4`}>
       {/* {console.log(dataBaseUsers, dataBaseAuth)} */}
       <div className="flex flex-wrap -mx-3 mb-6">
-        {/* <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-first-name"
-          >
-            User Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder="Jane"
-            onChange={(e) => {
-              const value = e.target.value;
-              setUserName(value);
-              usernameCondition(value);
-            }}
-          />
-          <p className="text-red-500 text-xs italic">
-            {usernameErr && usernameErr}
-          </p>
-        </div> */}
-        <div className="w-full md:w-1/2 px-3">
+        {isCreateAcc && (
+          <div className="w-full px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              User Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+              onChange={(e) => {
+                const value = e.target.value;
+                setUserName(value);
+                usernameCondition(value);
+              }}
+            />
+            <p className="text-red-500 text-xs italic">
+              {usernameErr && usernameErr}
+            </p>
+          </div>
+        )}
+        <div className="w-full px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-last-name"
@@ -306,6 +309,7 @@ export default function LogIn() {
           >
             Create Here!
           </button>
+          {formDataToShow}
         </p>
       ) : (
         <p
