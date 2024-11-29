7620fe5da10f3f985bf079fbc632bc791a8af6d1
Felisong <carolinahs100@gmail.com>
2024-11-29 00:15:55 -0700

    going to leave the story section alone for now.

--- app/components/CreateAcc.js
+++
@@ -1,3 +0,0 @@
-export default function CreateAcc() {
-  return <p> hello</p>;
-}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Hero from "../app/components/Hero/Hero";
+import OurStory from "./components/OurStory";
 
 export default function Home() {
   return (
@@ -10,19 +11,19 @@ export default function Home() {
         </main>
       </div>
       <section className="text-lg p-8 text-center md:px-60">
-        <>
-          <p className="">
-            Welcome to Golden Wheat Bakery, your local haven for handcrafted
-            artisan bread, delectable pastries, and savory buns.
-          </p>
-          <p>
-            We pride ourselves on using the finest quality ingredients and
-            traditional baking methods to create delicious treats that bring joy
-            to every bite. Explore our offerings and discover the taste of
-            freshly baked goodness that has made us a beloved part of the
-            community!
-          </p>
-        </>
+        <p className="">
+          Welcome to Golden Wheat Bakery, your local haven for handcrafted
+          artisan bread, delectable pastries, and savory buns.
+        </p>
+        <p>
+          We pride ourselves on using the finest quality ingredients and
+          traditional baking methods to create delicious treats that bring joy
+          to every bite. Explore our offerings and discover the taste of freshly
+          baked goodness that has made us a beloved part of the community!
+        </p>
+      </section>
+      <section>
+        <OurStory />
       </section>
     </>
   );
