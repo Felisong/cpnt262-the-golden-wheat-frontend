29baca75f9e12a7265b03e4804863360d9a59620
SFKia359 <ethanmajor117@gmail.com>
2024-11-29 00:12:03 -0700

    Got the cards working on moblie, tablet, and desktop. Have to get the api working

--- app/components/Cards.js
+++ app/components/Cards.js
@@ -2,15 +2,17 @@
 
 export default function Cards() {
   return (
-    <div className="w-11/12 md:w-80 px-1 py-1 m-4    rounded-t-3xl overflow-hidden shadow-lg bg-brownBg">
+    <div className="w-11/12 md:w-10/12 p-1 m-4 md:m-2 rounded-t-3xl overflow-hidden shadow-lg bg-brownBg">
       <img
         className="w-full rounded-t-3xl"
         src="/bread1.png"
         alt="Sunset in the mountains"
       />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-rose-500 text-base flex justify-center">
+        <div className="text-yellowBright font-bold text-xl mb-2">
+          The Coldest Sunset
+        </div>
+        <p className="text-yellowBright text-base flex justify-center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
--- app/components/GroupCards.js
+++ app/components/GroupCards.js
@@ -1,8 +1,31 @@
-const { default: Cards } = require("./Cards");
+"use client";
+import Cards from "./Cards";
+import { useEffect, useState } from "react";
 
 export default function GroupCards() {
+  async function fetchProducts() {
+    try {
+      const url = "http://localhost:5000/api/products";
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`response status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      console.log(`data: `, data);
+      setProducts(data);
+    } catch (error) {
+      console.log("unable to fetch data", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="flex-col md:flex-row">
+    <div className="w-fit h-fit flex">
       <Cards
         imgSrc={"/bread1.png"}
         Title={"The Coldest Sunset"}
