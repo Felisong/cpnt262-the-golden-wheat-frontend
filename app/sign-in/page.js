28fbc8505ee5300e59911b492421c6785f12a6a8
Felisong <carolinahs100@gmail.com>
2024-11-28 19:49:39 -0700

    user created successfullygit add .! Now both sign in and user creation work. I will make it so if its successful, I'll route to the dashboard

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -7,7 +7,7 @@ export default function SignIn() {
     <>
       <div className="flex justify-center">
         <div
-          className={` bg-yellowBright bg-no-repeat bg-contain bg-center rounded-lg flex-col m-4 w-10/12`}
+          className={` bg-yellowBright bg-no-repeat bg-contain bg-center rounded-lg flex-col m-4 w-10/12 md:w-1/2 lg:w-1/3`}
         >
           <LogIn />
         </div>
