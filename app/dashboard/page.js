d8fc0a48f5e5d9e4fbbcc858385565859b668186
Felisong <carolinahs100@gmail.com>
2024-11-29 23:14:06 -0700

    got dashboard hero looking how I want it to.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -9,9 +9,9 @@ export default function DashBoard() {
   return (
     <>
       <section
-        className={`bg-[url('/dashboard.jpg')] bg-cover bg-center h-[70vh] `}
+        className={`bg-[url('/dashboard.jpg')] bg-cover bg-center h-[70vh] relative`}
       >
-        <h1 className="text-xl text-center mt-32 font-lexendExa">
+        <h1 className="absolute text-4xl font-lexendExa top-64 m-4 md:top-48 lg:top-64">
           {`Hello ${username ? username : "user"}`}
         </h1>
       </section>
